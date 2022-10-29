1e3ad78afc443d6e08e5e3d55cd11af75221e09c
zemlyanuhin_mv <zemlyanuhin_mv@magnit.ru>
2022-10-30 00:43:52 +0300

    fix

--- exercise_1.py
+++ exercise_1.py
@@ -1,26 +1,38 @@
 """
 Задача 1.   Даны два списка,  нужно вернуть элементы, которые есть в 1-ом списке, но нет во 2-ом. Оценить эффективность своего решения.
-Для проверки эффективности алгоритма используем библиотеку timeit, чтоб засечь время выполнения каждой функции.
+
+Для алгоритмов используем библиотеку timeit, чтоб засечь время выполнения каждой функции.
+Сложность O(i + y): i, y - количество элементов в первом и во втором списках соответственно.
 """
 import timeit
 
-def func1():
+
+def func1(A: list, B: list) -> list:
     '''
-    С использованием стандартного оператора in
-    для поиска по списку.
+    Преобразуем второй список во множество.
+    Используем стандартный оператор in для поиска по списку.
     :return: list
     '''
-    A = [str(n) for n in range(1000)]
-    B = [str(n) for n in range(100,1100)]
+    setB = set(B)
+    index = 0
+    for i in A:
+        if i not in setB:
+            A[index] = i
+            index += 1
+    return A[:index]
+
+def func2(A: list, B: list) -> list:
+    '''
+    С использованием стандартного оператора in для поиска по списку.
+    :return: list
+    '''
     return [i for i in A if i not in B]
 
-def func2():
+def func3(A: list, B: list) -> list:
     '''
     С использованием функции any
     :return: list
     '''
-    A = [str(n) for n in range(1000)]
-    B = [str(n) for n in range(100,1100)]
     C = []
     for i in A:
         if not any(element in i for element in B):
@@ -28,13 +40,11 @@ def func2():
     return C
 
 
-def func3():
+def func4(A: list, B: list) -> list:
     '''
     Поиск с помощью перебора в цикле
     :return: list
     '''
-    A = [str(n) for n in range(1000)]
-    B = [str(n) for n in range(100,1100)]
     C = []
     for i in A:
         flag = False
@@ -45,36 +55,31 @@ def func3():
             C.append(i)
     return C
 
-def func4():
-    '''
-    Проверяем, сможем ли ещё ускорить процесс применив функцию map для генерации списка.
-    :return: list
-    '''
-    A = list(map(str, range(1000)))
-    B = list(map(str, range(100,1100)))
-    return [i for i in A if i not in B]
 
 if __name__ == '__main__':
+    A = [str(n) for n in range(1000)]
+    B = [str(n) for n in range(100, 1100)]
+
     print("# Вариант №1 -------------------")
     start_time = timeit.default_timer()
-    C = func1()
+    C = func1(A, B)
     print(C)
     print('Время выполнения: ', timeit.default_timer() - start_time)
 
     print("# Вариант №2 -------------------")
     start_time = timeit.default_timer()
-    C = func2()
+    C = func2(A, B)
     print(C)
     print('Время выполнения: ', timeit.default_timer() - start_time)
 
     print("# Вариант №3 -------------------")
     start_time = timeit.default_timer()
-    func3()
+    C = func3(A, B)
     print(C)
     print('Время выполнения: ', timeit.default_timer() - start_time)
 
     print("# Вариант №4 -------------------")
     start_time = timeit.default_timer()
-    func4()
+    C = func4(A, B)
     print(C)
     print('Время выполнения: ', timeit.default_timer() - start_time)
